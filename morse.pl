a44d52edccb07a4463448b8f1058badff76de556
Anas Rchid <rchid.anas@gmail.com>
2018-01-28 19:07:29 +0000

    add description..

--- morse.pl
+++ morse.pl
@@ -5,17 +5,21 @@
 #
 #        USAGE: ./morse.pl
 #
-#  DESCRIPTION: ---
+#  DESCRIPTION: this was my final project when i was at ISTA, so i have a some
+#		sort of mermories when i hear `morse code`, good memories
+#		indeed.
+#		so i was bored while i was thinking: "why no re-implement morse
+#		project using perl?".
 #
-#      OPTIONS: ---
-# REQUIREMENTS: ---
+#		the result was shoking, in less than 40 i re-create a project
+#		toke me more than 50 days of work! i'm not sure is it Perl that
+#		makes stuff fucking easy or is it me who have become a better
+#		programmer? who knows..
+#
 #         BUGS: ---
-#        NOTES: ---
 #       AUTHOR: Anas Rchid (0x0584) <rchid.anas@gmail.com>
-# ORGANIZATION: ---
 #      VERSION: 1.0
 #      CREATED: 01/27/2018
-#     REVISION: ---
 #===============================================================================
 
 use strict;
